a184293cf652341664fd01b04336c04b488dce82
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2024-12-04 12:03:07 +0530

    Fixed the issue with calling the report function properly

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 // import { getURLsfromHTML } from "./crawl.js";
 // import { argv } from 'node:process';
 import { crawlPage } from "./crawl.js";
+import { reportpages } from "./report.js";
 
 
 
 // getURLsfromHTML("<a href=\"blog\">Learn Backend Development</a>", 'https://boot.dev');
 
-function main() {
+async function main() {
     //process.argv contains the cmd line arguments
     //First two arguments are always:
     // - path to Nodejs executable
@@ -28,8 +29,21 @@ function main() {
 
     // base url = https://wagslane.dev
     const baseURl = args[0];
-    console.log(`Starting Crawling at: ${baseURl}`);
-    crawlPage(baseURl);
 
+    try {
+        new URL(baseURl);
+        console.log(`Starting crawler at: ${baseURl}`);
+        const pages = await crawlPage(baseURl);
+        reportpages(pages);
+
+
+
+    } catch (error) {
+        console.log('Error: Invalid URL format');
+        process.exit(1);
+    }
 }
 main()
+
+// Console.log(`Starting Crawling at: ${baseURl}`);
+// crawlPage(baseURl);
